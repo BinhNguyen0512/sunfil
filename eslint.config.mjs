7d820a84def90a95d4211412b8cb6ba9f993fd14
Binh Nguyen <binh.nguyen@atech88.com>
2025-05-26 18:42:07 +0700

    feat: setup eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier", "simple-import-sort"],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "react-hooks/exhaustive-deps": 0,
+      "no-unused-vars": "warn",
+      "prefer-const": "off",
+    },
+  }),
+];
+
+export default eslintConfig;
